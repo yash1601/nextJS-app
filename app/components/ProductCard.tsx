89b48fbccb656b9ac15d4f6f93548674ad85d9d7
Yash Goplani <yash.goplani@mastercard.com>
2024-03-21 01:33:41 +0530

    fetching, rendering, tailwind

---
+++ app/components/ProductCard.tsx
@@ -0,0 +1,15 @@
+'use client';
+import React from 'react'
+import AddToCart from './AddToCart';
+import styles from './ProductCard.module.css';
+
+
+const ProductCard = () => {
+  return (
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-600'>
+        <AddToCart />
+    </div>
+  )
+}
+
+export default ProductCard
