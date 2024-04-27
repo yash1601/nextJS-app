47b5c03d9aa65765b683049f128413ffbed09266
Yash Goplani <yash.goplani@mastercard.com>
2024-04-27 12:39:27 +0530

    added carousel for images

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import ProductCard from "./components/ProductCard";
+import EmblaCarousel from "./components/Carousel";
+import { EmblaOptionsType } from 'embla-carousel'
+import Autoplay from 'embla-carousel-autoplay'
 
+const OPTIONS: EmblaOptionsType = { loop: true }
+const SLIDE_COUNT = 5
+const SLIDES = Array.from(Array(SLIDE_COUNT).keys())
 export default function Home() {
   return (
     <main>
@@ -10,6 +16,9 @@ export default function Home() {
         <img src="/Home.png" alt="" className="w-full h-[36rem]"/>
         </div>
       </header>
+      <div>
+        <EmblaCarousel slides={SLIDES} options={OPTIONS} />
+      </div>
     </main>
   );
 }
